ecf6d746ab94b4ca1c36bc8e756ebda8af638c03
Nguyễn Quang Vinh <huntervin06@gmail.com>
2026-06-10 15:28:49 +0700

    Sua connection sang pyodbc

--- backend/app/database/connection.py
+++ backend/app/database/connection.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
 import os
+import urllib
 
 load_dotenv()
 
@@ -10,12 +11,16 @@ DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USERNAME")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 
-
-DATABASE_URL = (
-    f"mssql+pymssql://{DB_USER}:{DB_PASSWORD}"
-    f"@{DB_SERVER}/{DB_NAME}"
+params = urllib.parse.quote_plus(
+    f"DRIVER={{ODBC Driver 17 for SQL Server}};"
+    f"SERVER={DB_SERVER};"
+    f"DATABASE={DB_NAME};"
+    f"UID={DB_USER};"
+    f"PWD={DB_PASSWORD};"
+    f"TrustServerCertificate=yes;"
 )
 
+DATABASE_URL = f"mssql+pyodbc:///?odbc_connect={params}"
 
 engine = create_engine(
     DATABASE_URL,
