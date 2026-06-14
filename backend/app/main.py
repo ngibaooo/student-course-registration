e823d8a5f911570496ac1dae36152e050e559604
GiaBaoo <ngbao840@gmail.com>
2026-06-15 02:02:49 +0700

    declare registration_router in main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
-from app.api.admin_registration import (router as admin_registration_router)
-from app.api.admin_student import router as admin_student_router
-from app.api.admin_course import router as admin_course_router
-from app.api.admin_semester import router as admin_semester_router
-from app.api.admin_course_section import (router as admin_course_section_router)
+from app.api.admin.admin_registration import (router as admin_registration_router)
+from app.api.admin.admin_student import router as admin_student_router
+from app.api.admin.admin_course import router as admin_course_router
+from app.api.admin.admin_semester import router as admin_semester_router
+from app.api.admin.admin_course_section import (router as admin_course_section_router)
+from app.api.student.registration_api import router as registration_router
 app = FastAPI()
 
 app.include_router(admin_student_router)
@@ -11,3 +12,4 @@ app.include_router(admin_course_router)
 app.include_router(admin_semester_router)
 app.include_router(admin_course_section_router)
 app.include_router(admin_registration_router)
+app.include_router(registration_router)
