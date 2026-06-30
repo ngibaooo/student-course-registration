867aa51d7e9d7c8df4ba6cb0b62a38606d1c7193
GiaBaoo <ngbao840@gmail.com>
2026-06-30 20:16:54 +0700

    update vw_student_schedule.sql: render course section with the correct week

--- database/views/vw_student_schedule.sql
+++ database/views/vw_student_schedule.sql
@@ -1,7 +1,6 @@
-IF OBJECT_ID('vw_student_schedule', 'V') IS NOT NULL
-    DROP VIEW vw_student_schedule;
+IF OBJECT_ID('dbo.vw_student_schedule', 'V') IS NOT NULL
+    DROP VIEW dbo.vw_student_schedule;
 GO
-
 CREATE VIEW vw_student_schedule
 AS
 SELECT
@@ -22,7 +21,7 @@ SELECT
     sem.semester_name,
     sem.academic_year,
     sem.start_date,
-    sem.end_date 
+    sem.end_date
 
 FROM CourseRegistration cr
 
@@ -41,8 +40,7 @@ JOIN Course c
 JOIN Semester sem
     ON cs.semester_id = sem.id
 
-LEFT JOIN Lecturer l ON cs.lecturer_id = l.id
+LEFT JOIN Lecturer l
+    ON cs.lecturer_id = l.id
 
-WHERE cr.status = 'REGISTERED'
-AND GETDATE() BETWEEN sem.start_date AND sem.end_date;
-GO
+WHERE cr.status = 'REGISTERED';
