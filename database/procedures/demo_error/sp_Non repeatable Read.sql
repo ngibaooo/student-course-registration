598755974ad5be095de423a87714b0044f3835d3
Nguyễn Quang Vinh <huntervin06@gmail.com>
2026-07-04 20:41:16 +0700

    Doi ten file

--- database/procedures/demo_error/sp_Non repeatable Read.sql
+++ database/procedures/demo_error/sp_Non repeatable Read.sql
@@ -7,7 +7,11 @@ CREATE PROCEDURE sp_demo_non_repeatable_read_admin
 AS
 BEGIN
     -- Mức cô lập READ COMMITTED (mặc định) cho phép xảy ra Non-repeatable Read
-    SET TRANSACTION ISOLATION LEVEL READ COMMITTED;
+
+    --SET TRANSACTION ISOLATION LEVEL READ COMMITTED;
+    
+    --Fix LOI
+    SET TRANSACTION ISOLATION LEVEL REPEATABLE READ;
     
     DECLARE @Read1 INT;
     DECLARE @Read2 INT;
